e0108c885a66c81504c1ef109290f3cd6b83cc7f
Jaime Aranzazu <jaedaraz@gmail.com>
2023-06-09 15:19:12 -0500

    Remove unnecessary get field lambda

--- src/functions/getField/index.js
+++
@@ -1,7 +0,0 @@
-import FieldsService from "../../common/services/FieldsService.js"
-
-export const handler = async (event) => {
-  const fieldId = event.pathParameters.fieldId;
-  const fieldsService = new FieldsService();
-  return fieldsService.getField(fieldId);
-}

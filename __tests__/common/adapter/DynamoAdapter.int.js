664c164bbcb4ef50c49e288f31e8c9e92c370a98
Jaime Aranzazu <jaedaraz@gmail.com>
2023-06-12 11:50:14 -0500

    Added extra test case for dynamo adapter

--- __tests__/common/adapter/DynamoAdapter.int.js
+++ __tests__/common/adapter/DynamoAdapter.int.js
@@ -1,5 +1,4 @@
 import DynamoAdapter from "../../../src/common/adapter/DynamoAdapter.js";
-import Field from "../../../src/common/entities/Field.js";
 
 describe("DynamoDB Adapter", () => {
   const futBookingTableName = process.env.futBookingTableName;
@@ -17,18 +16,21 @@ describe("DynamoDB Adapter", () => {
     expect(result.ConsumedCapacity.TableName).toBe(futBookingTableName);
   });
 
-  it("Create and delete item", async () => {
+  it("Create, get and delete item", async () => {
     const db = new DynamoAdapter();
     const PK = "TEST-PK";
     const SK = "TEST-SK";
     const objectCreate = { PK, SK };
 
     const createResult = await db.createItem(futBookingTableName, objectCreate);
+    const getResult = await db.getByKey(futBookingTableName, PK, SK);
     const deleteResult = await db.deleteItem(futBookingTableName, PK, SK);
     const check = await db.getByKey(futBookingTableName, PK, SK);
 
     expect(createResult.ConsumedCapacity.TableName).toBe(futBookingTableName);
     expect(deleteResult.ConsumedCapacity.TableName).toBe(futBookingTableName);
+    expect(getResult.Item.PK).toBe(PK);
+    expect(getResult.Item.SK).toBe(SK);
     expect(check.Item).toBe(undefined);
   });
 })

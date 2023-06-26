5cc803f332233286d8893f0d9eabbc507222ef4d
Jaime Aranzazu <jaedaraz@gmail.com>
2023-06-25 21:05:02 -0500

    Add region

--- config/getAuthToken.sh
+++ config/getAuthToken.sh
@@ -2,6 +2,7 @@ USERNAME=$(node config/credsGenerator.js GENERATE_EMAIL)
 PASSWORD=$(node config/credsGenerator.js GENERATE_PASS)
 
 COGNITO_USER_SUB=$(aws cognito-idp admin-create-user \
+  --region us-east-1 \
   --username $USERNAME \
   --user-pool-id $USER_POOL_ID \
   --user-attributes Name=email,Value=$USERNAME Name=name,Value="Jhon Doe" Name=gender,Value=M Name=birthdate,Value="10/10/2010" \
@@ -13,12 +14,14 @@ echo "user sub"
 echo $COGNITO_USER_SUB
 
 aws cognito-idp admin-set-user-password \
+  --region us-east-1 \
   --user-pool-id $USER_POOL_ID \
   --username $USERNAME \
   --password $PASSWORD \
   --permanent
 
 ID_TOKEN=$(aws cognito-idp initiate-auth \
+  --region us-east-1 \
   --client-id $USER_POOL_CLIENT_ID \
   --auth-flow USER_PASSWORD_AUTH \
   --auth-parameters USERNAME=$USERNAME,PASSWORD=$PASSWORD \

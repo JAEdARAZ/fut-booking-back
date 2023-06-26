8d67b955a34aa22d8eec97e081e98772b3a9e6d6
Jaime Aranzazu <jaedaraz@gmail.com>
2023-06-25 21:15:40 -0500

    Use region present in awsenv

--- config/getAuthToken.sh
+++ config/getAuthToken.sh
@@ -1,8 +1,9 @@
+source .awsenv
 USERNAME=$(node config/credsGenerator.js GENERATE_EMAIL)
 PASSWORD=$(node config/credsGenerator.js GENERATE_PASS)
 
 COGNITO_USER_SUB=$(aws cognito-idp admin-create-user \
-  --region us-east-1 \
+  --region $region \
   --username $USERNAME \
   --user-pool-id $USER_POOL_ID \
   --user-attributes Name=email,Value=$USERNAME Name=name,Value="Jhon Doe" Name=gender,Value=M Name=birthdate,Value="10/10/2010" \
@@ -10,26 +11,20 @@ COGNITO_USER_SUB=$(aws cognito-idp admin-create-user \
   --output text \
   --query User.Username)
 
-echo "user sub"
-echo $COGNITO_USER_SUB
-
 aws cognito-idp admin-set-user-password \
-  --region us-east-1 \
+  --region $region \
   --user-pool-id $USER_POOL_ID \
   --username $USERNAME \
   --password $PASSWORD \
   --permanent
 
 ID_TOKEN=$(aws cognito-idp initiate-auth \
-  --region us-east-1 \
+  --region $region \
   --client-id $USER_POOL_CLIENT_ID \
   --auth-flow USER_PASSWORD_AUTH \
   --auth-parameters USERNAME=$USERNAME,PASSWORD=$PASSWORD \
   --query AuthenticationResult.IdToken \
   --output text)
-
-echo "id token"
-echo $ID_TOKEN
 
 echo "" >> .awsenv
 echo "cognitoUserIdToken=$ID_TOKEN" >> .awsenv

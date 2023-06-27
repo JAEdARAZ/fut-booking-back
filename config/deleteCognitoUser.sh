e2a1d698ef52f8d564508f7d9e70d2c2ea54452d
Jaime Aranzazu <jaedaraz@gmail.com>
2023-06-26 21:32:15 -0500

    Test deletion of cognito user

--- config/deleteCognitoUser.sh
+++ config/deleteCognitoUser.sh
@@ -1,7 +1,9 @@
 source .awsenv
 source .slsoutput
 
+echo "before admin-delete-user"
 aws cognito-idp admin-delete-user \
   --region $region \
   --user-pool-id $USER_POOL_ID \
   --username $congitoUsername
+echo "after admin-delete-user"

e4e927c9ea8a24509586aabcccec611b0e27889d
Jaime Aranzazu <jaedaraz@gmail.com>
2023-06-26 21:37:57 -0500

    Add log after cognito user deletion

--- config/deleteCognitoUser.sh
+++ config/deleteCognitoUser.sh
@@ -1,9 +1,9 @@
 source .awsenv
 source .slsoutput
 
-echo "before admin-delete-user"
 aws cognito-idp admin-delete-user \
   --region $region \
   --user-pool-id $USER_POOL_ID \
   --username $congitoUsername
-echo "after admin-delete-user"
+
+echo "Deleted user $congitoUsername"
